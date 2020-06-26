763bc1c1bbe4fa2f9c70fa758e5b3672d7e94e10
Matthew Collier <mcollier@digitalbazaar.com>
2020-06-26 19:34:36 -0400

    Add core files.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+import {httpClient} from '@digitalbazaar/http-client';
 
-import confetti from 'canvas-confetti';
+async function getSomething() {
+  const x = await httpClient.get('https://httpbin.org/json');
+  console.log(JSON.stringify(x, null, 2));
+}
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+getSomething();
