ad06a2ad12620eb4fefcb954e72613477b0e29ce
Matthew Collier <mcollier@digitalbazaar.com>
2020-06-26 20:11:33 -0400

    More changes.

--- build/_dist_/index.js
+++
@@ -1,6 +0,0 @@
-import {httpClient} from "/web_modules/@digitalbazaar/http-client.js";
-async function getSomething() {
-  const x = await httpClient.get("https://httpbin.org/json");
-  console.log(JSON.stringify(x, null, 2));
-}
-getSomething();
